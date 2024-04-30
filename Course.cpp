1f54ed4ba0220aba8a46fd5c870d41e644874daa
Kevin06x <kevinzhang06x@gmail.com>
2024-04-30 14:22:54 -0700

    Input

---
+++ Course.cpp
@@ -0,0 +1,43 @@
+/*
+	Bastida Marin, Sebastian (Team Leader)
+	Lee, Kevin
+	Zhang, Kevin
+
+	Project: Grade Report
+	CS A250
+	Fall 2023
+*/
+
+#include "Course.h"
+
+using namespace std;
+
+void Course::setCourseInfo(const string& paramCoursePrefix, 
+	int paramCourseNumbers, int paramCourseUnits)
+{
+	coursePrefix = paramCoursePrefix;
+	courseNumber = paramCourseNumbers;
+	courseUnits = paramCourseUnits;
+}
+
+string Course::getCoursePrefix() const
+{
+	return coursePrefix;
+}
+
+int Course::getCourseNumber() const
+{
+	return courseNumber;
+}
+
+int Course::getCourseUnits() const
+{
+	return courseUnits;
+}
+
+bool Course::operator<(const Course& otherCourse) const
+{
+	return ((coursePrefix != otherCourse.coursePrefix) ?
+		coursePrefix < otherCourse.coursePrefix :
+		courseNumber < otherCourse.courseNumber);
+}
